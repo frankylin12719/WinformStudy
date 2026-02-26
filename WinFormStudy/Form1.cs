ebaa75bb0c8a6e0a5d493eccacc3e2c225c71b89
franky <2971684021@qq.com>
2026-02-26 16:18:26 +0800

    feat: 代码生成按钮

--- WinFormStudy/Form1.cs
+++ WinFormStudy/Form1.cs
@@ -2,9 +2,25 @@ namespace WinFormStudy
 {
     public partial class Form1 : Form
     {
+        Button btn;
         public Form1()
         {
             InitializeComponent();
+            btn = new()
+            {
+                Text = "OK new",
+                Location = new Point(20, 20),
+                Width = 100,
+                Height = 100
+            };
+            btn.Click += Btn_Click;
+            this.Controls.Add(btn);
+        }
+
+
+        private void Btn_Click(object? sender, EventArgs e)
+        {
+            btn.Text = "OKK";
         }
 
         private void button1_Click(object sender, EventArgs e)
