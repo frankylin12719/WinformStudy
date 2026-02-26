b307ca436bbbb261719c0c2fa36eaf7526aeb831
franky <2971684021@qq.com>
2026-02-26 16:41:44 +0800

    feat: 按钮循环

--- WinFormStudy/Form1.cs
+++ WinFormStudy/Form1.cs
@@ -15,8 +15,29 @@ namespace WinFormStudy
             };
             btn.Click += Btn_Click;
             this.Controls.Add(btn);
+            for (int i = 0; i < 4; i++)
+            {
+                Button buttonLoop = new()
+                {
+                    Text = "btn" + i,
+                    Location = new Point(100 + i * 40, 100 + i * 50),
+                    Width = 150,
+                    Height = 50
+                };
+                buttonLoop.Click += ButtonLoop_Click;
+                this.Controls.Add(buttonLoop);
+            }
         }
 
+        private void ButtonLoop_Click(object? sender, EventArgs e)
+        {
+            
+            if (sender != null)
+            {
+                Button btnLoop = (Button)sender;
+                btnLoop.Text = "Clicked";
+            }
+        }
 
         private void Btn_Click(object? sender, EventArgs e)
         {
